fac1519c0c5a46b24db822d93ac8078e140947ee
Brian Lacey <ss3teddy@gmail.com>
2018-05-09 15:08:29 -0700

    Refactored basic API endpoints.

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const router = require("./workoutLog.routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use("/", router);
 
-app.post("/", (req, res)=> res.status(200).send("I post!"));
-app.get("/", (req, res)=> res.status(200).send("I get!"));
-// app.get("/:id", (req, res)=> res.status(200).send("I get by id!"));
-app.put("/", (req, res)=> res.status(200).send("I put!"));
-app.delete("/", (req, res)=> res.status(200).send("I delete!"));
+// app.post("/", (req, res)=> res.status(200).send("I post!"));
+// app.get("/", (req, res)=> res.status(200).send("I get!"));
+// // app.get("/:id", (req, res)=> res.status(200).send("I get by id!"));
+// app.put("/", (req, res)=> res.status(200).send("I put!"));
+// app.delete("/", (req, res)=> res.status(200).send("I delete!"));
 
 app.listen(3000);
