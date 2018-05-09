c0697c6d0305cbf10d5d2012f01093182c59eb99
Brian Lacey <ss3teddy@gmail.com>
2018-05-09 13:12:25 -0700

    first commit to github.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.post("/", (req, res)=> res.status(200).send("I post!"));
+app.get("/", (req, res)=> res.status(200).send("I get!"));
+// app.get("/:id", (req, res)=> res.status(200).send("I get by id!"));
+app.put("/", (req, res)=> res.status(200).send("I put!"));
+app.delete("/", (req, res)=> res.status(200).send("I delete!"));
+
+app.listen(3000);
