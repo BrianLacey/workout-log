e73e7f4131c9aadf4d322dbbb355b712f839d50c
Brian Lacey <ss3teddy@gmail.com>
2018-05-21 17:51:29 -0700

    Removed stray debuggers.

--- app/validate/apiValidation.js
+++ app/validate/apiValidation.js
@@ -2,9 +2,7 @@ const Joi = require("joi");
 const exerciseSchema = require("../schema/exerciseSchema");
 
 module.exports = (req, res, next) => {
-    // debugger;
     Joi.validate(req.body, exerciseSchema, (err, value) => {
-      // debugger;
       if (err) {
         res.status(400).send(err);
       } else {
